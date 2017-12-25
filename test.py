cc568772bd34f66660a03fec2bed87183d27bd95
zhangweiwang <zhangweiwang@pwrd.cn>
2017-12-25 11:51:01 +0800

    update

--- test.py
+++ test.py
@@ -1,31 +1,11 @@
-class A(object):
-    va = 10
+import json
 
-    def foo(self):
-        self.va = 10
-        print(A.va)
-        print(self.va)
+fileName = "resource/test.txt"
 
-        self.va = 40
-        print(A.va)
-        print(self.va)
-
-        va = 20
-        print(va)
-
-        A.va = 15
-        print(A.va)
-        print(self.va)
-
-
-        # Script starts from here
-
-
-obj1 = A()
-obj2 = A()
-obj1.foo()
-obj2.foo()
-print(A.va)
-
-print(obj1.va)
-print(obj2.va)
+fopen = open(fileName, 'a+')
+lines = fopen.readlines()
+lines.append("testtest")
+print(lines)
+str = json.dumps(lines)
+fopen.write(str)
+fopen.flush()
